ec70dcd006d57010c2e9b9cd6ed10c406b85bd11
曾荣富 <hankock@123.local>
2026-06-28 21:35:09 +0800

    fix db duplicate declaration

--- js/core/db.js
+++ js/core/db.js
@@ -1,49 +1,22 @@
 // js/core/db.js
-const DB_NAME = "fire-crm";
-let db;
+
+let dbInstance = null;
 
 export async function initDB() {
-  db = await new Promise((resolve) => {
-    const request = indexedDB.open(DB_NAME, 1);
+  if (dbInstance) return dbInstance;
+
+  dbInstance = await new Promise((resolve) => {
+    const request = indexedDB.open("fire-crm", 1);
 
     request.onupgradeneeded = () => {
-      const d = request.result;
-
-      d.createObjectStore("sku", { keyPath: "id" });
-      d.createObjectStore("customer", { keyPath: "id" });
-      d.createObjectStore("invoice", { keyPath: "id" });
+      const db = request.result;
+      db.createObjectStore("sku", { keyPath: "id" });
+      db.createObjectStore("customer", { keyPath: "id" });
+      db.createObjectStore("invoice", { keyPath: "id" });
     };
 
     request.onsuccess = () => resolve(request.result);
   });
+
+  return dbInstance;
 }
-
-export const dbAPI = {
-  async getAll(store) {
-    return new Promise(res => {
-      const tx = db.transaction(store, 'readonly');
-      const req = tx.objectStore(store).getAll();
-      req.onsuccess = () => res(req.result);
-    });
-  },
-
-  async get(store, id) {
-    return new Promise(res => {
-      const tx = db.transaction(store, 'readonly');
-      const req = tx.objectStore(store).get(id);
-      req.onsuccess = () => res(req.result);
-    });
-  },
-
-  async add(store, data) {
-    const tx = db.transaction(store, 'readwrite');
-    tx.objectStore(store).add(data);
-  },
-
-  async put(store, data) {
-    const tx = db.transaction(store, 'readwrite');
-    tx.objectStore(store).put(data);
-  }
-};
-
-export const db = dbAPI;
